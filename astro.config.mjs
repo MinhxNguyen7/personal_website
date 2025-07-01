cb3aec1393a3cff4ef4a9f31b6384cdf74f0a5b3
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-06-30 19:35:01 -0700

    ci: use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,28 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     drafts: true,
     shikiConfig: {
       theme: "css-variables"
     }
   },
+
   shikiConfig: {
     wrap: true,
     skipInline: false,
     drafts: true
   },
-  site: 'https://yourdomain.com',
-  integrations: [sitemap(), mdx()]
+
+  site: 'https://minh-nguyen.tech',
+  integrations: [sitemap(), mdx()],
+  output: 'static',
+  adapter: vercel()
 });
