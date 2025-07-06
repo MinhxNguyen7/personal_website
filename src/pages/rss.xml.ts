c8adc3ec781fe264e501af7403c537fb836e36ce
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-07-05 22:34:33 -0700

    fix(rss): items enumeration

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,22 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: { site: string }) {
+  const posts = await getCollection("posts")
+  posts.sort((a, b) => {
+    return new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime();
+  });
+
   return rss({
     title: "Minh Nguyen's writings",
     description: "Minh's writings on various topics.",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./src/content/posts/**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `${context.site}/blog/${post.slug}`,
+      pubDate: post.data.pubDate,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
